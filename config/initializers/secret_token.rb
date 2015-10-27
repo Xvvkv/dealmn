015a5948c133a417d2712830bc72d9b7e466e5d6
Enkhbold Tsagaach <enkhbold.tsagaach@gmail.com>
2015-10-27 16:32:57 +0800

    initializing new project

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Dealmn::Application.config.secret_token = '76c5b218661b20010d76b968beb1e40b04a36128250abe07caf8f0c0977d7645695125ac3fba024f9ac984b4665060d89e5e49a557202273347fb7b47d6103c1'
